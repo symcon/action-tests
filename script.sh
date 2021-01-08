c9d4cb840e75343ff1d06f30c10ffa6d58495a9f
TillBrede <51406030+TillBrede@users.noreply.github.com>
2021-01-08 11:20:44 +0100

    properly fail if file is not found

--- script.sh
+++ script.sh
@@ -1,13 +1,16 @@
 if [ -z $1 ]
 then
-  wget https://phar.phpunit.de/phpunit.phar
+  PHPUNIT=phpunit.phar
 else
-  wget https://phar.phpunit.de/phpunit-$1.phar
+  PHPUNIT=phpunit-$1.phar
 fi
 
-if [ -z $1 ]
-then
-  phpdbg -qrr phpunit.phar tests --configuration tests/phpunit.xml
+wget https://phar.phpunit.de/$PHPUNIT
+
+if [ -f "$PHPUNIT" ]
+then 
+  phpdbg -qrr $PHPUNIT tests --configuration tests/phpunit.xml 
 else
-  phpdbg -qrr phpunit-$1.phar tests --configuration tests/phpunit.xml
+  echo $PHPUNIT not found
+  exit 1
 fi
